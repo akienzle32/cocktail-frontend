4b9db9f1669e2222c1194e0fefdfb864ae52d3c7
Alec Kienzle <alec.kienzle@gmail.com>
2022-03-31 21:57:22 -0700

    added two columns for SearchByIngredient component

--- src/SearchByIngredients.tsx
+++ src/SearchByIngredients.tsx
@@ -1,9 +1,20 @@
 export function SearchByIngredients(){
     return (
-        <div>
-            <ul>
-                <button>Gin</button>
-            </ul>
+        <div className="flex items-center justify-center">
+            <div>
+                <p className="text-center">Ingredients</p>
+                <div className="w-72 h-80 bg-rose-500 pl-2 border border-solid border-x-white">
+                    <button className="block">Spirits</button>
+                    <button className="block">Liqueurs</button>
+                    <button className="block">Fruit juices</button>
+                </div>
+            </div>
+            <div>
+                <p className="text-center">My bar</p>
+                <div className="w-72 h-80 bg-rose-500 pl-2 border-solid border-r border-t border-b border-r-white">
+                    <button>Remove</button>
+                </div>
+            </div>
         </div>
     );
 }
