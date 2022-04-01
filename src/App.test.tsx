00e65fe8b6569ad0851e238d6583c88b2cf8336d
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-01 15:26:40 -0700

    added more tests for SearchByIngredients component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,8 +8,10 @@ import { Home } from './Home';
 test('SearchByName renders with Home page', () => {
   render(<MemoryRouter><Home /></MemoryRouter>);
   const searchByName = screen.getByRole('textbox');
+  const searchByIngredients = screen.queryByRole('button', {name: /spirits/i});
 
   expect(searchByName).toBeInTheDocument();
+  expect(searchByIngredients).not.toBeInTheDocument();
 });
 
 test('SearchByIngredients renders with button press', () => {
@@ -18,8 +20,11 @@ test('SearchByIngredients renders with button press', () => {
   const searchByIngredientsBtn = screen.getByRole('button', {name: /search by ingredients/i});
   fireEvent.click(searchByIngredientsBtn);
 
-  const searchByIngredientsComponent = screen.getByRole('button', {name: /spirits/i});
-  expect(searchByIngredientsComponent).toBeInTheDocument();
+  const searchByName = screen.queryByRole('textbox');
+
+  const spiritsBtn = screen.getByRole('button', {name: /spirits/i});
+  expect(spiritsBtn).toBeInTheDocument();
+  expect(searchByName).not.toBeInTheDocument();
 })
 
 test('Profile renders with link click', () => {
@@ -53,4 +58,59 @@ test('Back button renders after click of category', () => {
 
   const backButton = screen.getByRole('button', {name: /go back/i});
   expect(backButton).toBeInTheDocument();
+})
+
+test('Back button functions correctly', () => {
+  render(<MemoryRouter><App /></MemoryRouter>);
+  
+  const searchByIngredientsBtn = screen.getByRole('button', {name: /search by ingredients/i});
+  fireEvent.click(searchByIngredientsBtn);
+
+  const spiritsBtn = screen.getByRole('button', {name: /spirits/i});
+  fireEvent.click(spiritsBtn);
+
+  const backBtn = screen.getByRole('button', {name: /go back/i});
+  fireEvent.click(backBtn);
+
+  const nextSpiritsBtn = screen.queryByRole('button', {name: /spirits/i});
+  expect(nextSpiritsBtn).toBeInTheDocument();
+})
+
+test('Subcategories render after click of category', () => {
+  render(<MemoryRouter><App /></MemoryRouter>);
+
+  const searchByIngredientsBtn = screen.getByRole('button', {name: /search by ingredients/i});
+  fireEvent.click(searchByIngredientsBtn);
+
+  const spiritsBtn = screen.getByRole('button', {name: /spirits/i});
+  fireEvent.click(spiritsBtn);
+
+  const ginBtn = screen.queryByRole('button', {name: /gin/i});
+  expect(ginBtn).toBeInTheDocument();
+})
+
+test('MyBar ingredients persist after navigating to Profile', () => {
+  render(<MemoryRouter><App /></MemoryRouter>);
+
+  const searchByIngredientsBtn = screen.getByRole('button', {name: /search by ingredients/i});
+  fireEvent.click(searchByIngredientsBtn);
+
+  const spiritsBtn = screen.getByRole('button', {name: /spirits/i});
+  fireEvent.click(spiritsBtn);
+
+  const ginBtn = screen.getByRole('button', {name: /gin/i});
+  fireEvent.click(ginBtn);
+
+  const profileLink = screen.getByRole('link', {name: /my profile/i});
+  fireEvent.click(profileLink);
+
+  const homeLink = screen.getByRole('link', {name: /the cocktail library/i});
+  fireEvent.click(homeLink);
+
+  const nextSearchByIngredientsBtn = screen.getByRole('button', {name: /search by ingredients/i});
+  fireEvent.click(nextSearchByIngredientsBtn);
+
+  const nextGinBtn = screen.getByRole('button', {name: /gin/i});
+  expect(nextGinBtn).toBeInTheDocument();
+
 })
