2ddbd31d9f030b62257e1b09f087ac8ffff23dcd
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-01 11:05:19 -0700

    shrunk the margin between SearchBy buttons

--- src/Home.tsx
+++ src/Home.tsx
@@ -18,8 +18,8 @@ export function Home(){
     return(
         <div className="flex flex-col items-center justify-center">
             <div>
-                <button onClick={() => setSearchByName(true)} className="w-40 h-16 m-14 p-4 rounded bg-rose-500 hover:bg-rose-400 text-lg">Search by Name</button>
-                <button name="search-by-ingredients-btn" onClick={() => setSearchByName(false)} className="w-50 h-16 m-14 p-4 rounded bg-rose-500 hover:bg-rose-400 text-lg">Search by Ingredients</button>
+                <button onClick={() => setSearchByName(true)} className="w-40 h-16 m-10 p-4 rounded bg-rose-500 hover:bg-rose-400 text-lg">Search by Name</button>
+                <button name="search-by-ingredients-btn" onClick={() => setSearchByName(false)} className="w-50 h-16 m-10 p-4 rounded bg-rose-500 hover:bg-rose-400 text-lg">Search by Ingredients</button>
             </div>
             {searchComponent}
         </div>
