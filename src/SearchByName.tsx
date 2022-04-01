fd0ff368f8fbe6d516556d41d6f0572cba27fbbc
Alec Kienzle <alec.kienzle@gmail.com>
2022-03-31 21:34:04 -0700

    added search box to SearchByName component

--- src/SearchByName.tsx
+++ src/SearchByName.tsx
@@ -1,8 +1,22 @@
+import { useState, FormEvent, ChangeEvent } from 'react';
+
 export function SearchByName(){
-    return(
+    const [ cocktailSearch, setCocktailSearch ] = useState('');
+
+    function onSubmit(e: FormEvent): void {
+        e.preventDefault();
+    }
+
+    function handleChange(e: ChangeEvent): void {
+        const element = e.currentTarget as HTMLInputElement;
+        const value = element.value;
+        setCocktailSearch(value);
+    }
+
+    return (
         <div>
-            <form>
-                <input name="cocktail-name" className="pl-4" type="text" placeholder="Search for a cocktail..."></input>
+            <form onSubmit={onSubmit}>
+                <input onChange={handleChange} className="h-10 w-80 text-black text-center text-xl outline-rose-500" type="text" placeholder="Search for a cocktail..."></input>
             </form>
         </div>
     );
