41bee36d4585cc7dc03f0329dddda0e41160ccac
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-02 16:50:58 -0700

    added fetch request to CocktailDetail

--- src/CocktailDetail.tsx
+++ src/CocktailDetail.tsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { isPropertySignature } from "typescript";
+
 export function CocktailDetail(){
+    const location = useLocation();
+    const cocktailId = location.pathname.slice(1);
+
+    const [ details, setDetails ] = useState({});
+
+    // add fetch request here to get cocktail ingredients by id
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API}cocktails/${cocktailId}`, {
+            method: 'GET',
+            mode: 'cors',
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setDetails(data);
+        })
+    }, [])
+
     return (
         <div className="text-white">Detail</div>
     );
