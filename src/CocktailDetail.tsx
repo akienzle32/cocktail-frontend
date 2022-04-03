1f9d1c5329f65a8878778da32a3bf645872a8a4e
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-03 14:01:18 -0700

    styled CocktailDetail component

--- src/CocktailDetail.tsx
+++ src/CocktailDetail.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ReactElement } from "react";
 import { useLocation } from "react-router";
 import Cocktail from "./interfaces";
 
@@ -17,14 +17,62 @@ export function CocktailDetail(){
         })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             setDetails(data);
         })
     }, [])
 
+    function extractValuesFromJson(cocktailJson: Cocktail, key: string): Array<string>{
+        const entries: Array<Array<string>> = Object.entries(cocktailJson);
+        const values = entries.reduce((prev: Array<string>, curr: Array<string>) => {
+            let field: string;
+            if (curr[0].includes(key) && curr[1]){
+                field = curr[1];
+                prev.push(field);
+            }
+            return prev;
+        }, []);
+        return values;
+    }
+
+    function zip(ingredients: Array<string>, measures: Array<string>): Array<Array<string>>{
+        const tuples = ingredients.map((ingredient, index) => {
+            return [ingredient, measures[index]];
+        })
+        return tuples;
+    }
+
+    function createIngredientsAndMeasuresList(tuples: Array<Array<string>>): Array<ReactElement>{
+        const ingredientsAndMeasuresList = tuples.map((tuple: Array<string>, index) => {
+            if (tuple[1])
+                return <li className="ml-6" key={index}>{tuple[0]}: {tuple[1]}</li>;
+            else
+                return <li className="ml-6" key={index}>{tuple[0]}</li>;
+        })
+        return ingredientsAndMeasuresList;
+    }
+
     const name = details.name;
+    const image = details.image;
+    const instructions = details.instructions;
+    const ingredients: Array<string> = extractValuesFromJson(details, "ingredient");
+    const measures: Array<string> = extractValuesFromJson(details, "measure");
+    const ingredientsAndMeasures: Array<Array<string>> = zip(ingredients, measures);
+    const ingredientsAndMeasuresList = createIngredientsAndMeasuresList(ingredientsAndMeasures);
 
     return (
-        <div className="text-white">{name}</div>
+        <div className="overflow-scroll flex flex-col items-center justify-center">
+            <div className="w-1/3 pb-16">
+                <div className="flex flex-col items-center justify-center text-white">
+                    <div className="text-2xl mt-6 mb-4">{name}</div>
+                    <img src={image} width="350" height="425" />
+                </div>
+                <div className="flex flex-col items-start justify-start mt-4 ml-8 m text-white">
+                    <ul className="list-disc">
+                        {ingredientsAndMeasuresList}
+                    </ul>
+                    <div className="mt-4">{instructions}</div>
+                </div>
+            </div>
+        </div>
     );
 }
