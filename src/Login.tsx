f7d142a36d152ce28b27c4c323ce8b85f198e5c5
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-04 12:39:56 -0700

    adjusted color scheme for SearchByIngredients

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,18 +1,18 @@
 export function Login(){
     return (
-        <div className="flex flex-col items-center justify-center bg-red mt-8 w-1/3 pb-10 rounded">
+        <div className="flex flex-col items-center justify-center bg-darkred mt-8 w-1/3 pb-10 rounded">
             <div className="mt-6 mb-6 text-center">
                 <form className="flex flex-col items-start justify-center text-lg mt-4 text-black">
                     <label className="text-white" htmlFor="username">Username:</label>
-                    <input className="m-2 w-60 pl-1 rounded" type="text" id="username"></input>
+                    <input className="m-2 w-60 pl-1 rounded outline-cadetblue" type="text" id="username"></input>
                     <label className="text-white"htmlFor="password">Password:</label>
-                    <input className="m-2 w-60 pl-1 text-black rounded" type="password"></input>
+                    <input className="m-2 w-60 pl-1 text-black rounded outline-cadetblue" type="password"></input>
                 </form>
             </div>
-            <button className="w-32 text-lg text-white bg-cadetblue pl-4 pr-4 pt-1 pb-1.5 rounded-md hover:bg-white hover:text-red">Login</button>
+            <button className="w-32 text-lg text-white bg-cadetblue pl-4 pr-4 pt-1 pb-1.5 rounded-md hover:bg-lightcadetblue">Login</button>
             <br></br>
             <div className="text-lg font-bold mt-4 text-white">Or if you don't yet have an account,</div>
-            <button className="w-32 mt-4 pl-4 pr-4 pt-1 pb-1.5 text-lg font-bold rounded-md bg-cadetblue text-white hover:bg-white hover:text-red">Sign up</button>
+            <button className="w-32 mt-4 pl-4 pr-4 pt-1 pb-1.5 text-lg font-bold rounded-md bg-cadetblue text-white hover:bg-lightcadetblue">Sign up</button>
         </div>
     );
 }
