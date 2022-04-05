568507a1d96a1fc704f4ccd16106588220f51fb5
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-05 16:50:36 -0700

    moved fetch to profile endpoint to Search component

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,30 +1,9 @@
-import { useEffect } from "react";
-import { SavedCocktail } from "./interfaces";
 
 export function Profile(props: any){
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API}cocktails/profile`, {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Authorization': `Token ${props.token}`,
-            },
-            //credentials: 'include',
-        })
-        .then(request => request.json())
-        .then(data => {
-            let cocktailIdArray: Array<Number> = [];
-            data.forEach((object: SavedCocktail) => {
-                cocktailIdArray.push(object.cocktail);
-            })
-            props.setSavedCocktails(cocktailIdArray);
-        })
-    }, [])
-
-
     return (
         <div className="flex align-center justify-center">
-            <button>Profile</button>
+            <div>Profile</div>
+            <div>{props.savedCocktails}</div>
         </div>
     );
 }
