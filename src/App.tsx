9d0c32d6b6918a4b4a01c2b712c6bc99ca2104be
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-05 20:25:30 -0700

    added createCocktailLinks() to Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, ReactElement } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { Home } from './Home';
 import './App.css';
@@ -8,10 +8,11 @@ function App() {
 
   function displayLink(){
     if (!loggedIn)
-      return <Link to="/login" className="px-6 pb-1 rounded hover:bg-darkred">Login</Link>
+      return <Link to="/login" className="px-3 pb-1 mr-10 rounded hover:bg-darkred">Login</Link>
     else
-      return <Link to="/profile" className="px-6 pb-1 rounded hover:bg-darkred">My profile</Link>
+      return <Link to="/profile" className="px-3 pt-0.5 pb-1 mr-10 rounded hover:bg-darkred">My profile</Link>
   }
+
 
   const rightLink = displayLink();
 
@@ -20,7 +21,7 @@ function App() {
       <div className="h-20 bg-cadetblue text-white">
         <ul className="list-none">
           <li className="text-3xl float-left ml-16 mt-5"><Link to="/">The Cocktail Library</Link></li>
-          <li className="text-xl float-right mr-20 mt-6">{rightLink}</li>
+          <li className="text-xl float-right mr-4 mt-6">{rightLink}</li>
         </ul>
       </div>
       <Home loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
