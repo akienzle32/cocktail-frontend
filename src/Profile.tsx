91f08b5e5beec6cd30e92578957e13c926572944
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-06 09:53:06 -0700

    boldened page title

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -21,7 +21,7 @@ export function Profile(props: any){
     return (
         <div className="mt-10 w-full flex flex-col items-center justify-center">
             <div className="w-1/3 flex flex-col items-start justify-start pb-8 bg-darkred text-white rounded">
-                <div className="text-3xl mt-4 ml-44 w-full">My Profile</div>
+                <div className="text-3xl mt-4 ml-44 w-full font-extrabold">My Profile</div>
                 <div className="text-xl mt-8 ml-24">
                     <div className="bg-cadetblue py-4 pl-6 pr-8 rounded">
                         <div className="text-xl flex items-start justify-start"><div className="">Username:</div><div className="ml-16">{props.username}</div></div>
