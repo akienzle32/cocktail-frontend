0180b34579791780aefccac81489bb13a5e8333e
Alec Kienzle <alec.kienzle@gmail.com>
2022-04-06 16:40:20 -0700

    fixed bug with logout not resetting myBar

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,14 +11,14 @@ export function Home(props: any){
     const [ savedCocktails, setSavedCocktails ] = useState<Array<number>>([]);
     const [ searchByName, setSearchByName ] = useState<boolean>(true);
     const [ token, setToken ] = useState<string>();
-    const [ username, setUsername ] = useState<string>('');
+    const [ username, setUsername ] = useState<string>();
 
     // These functions can probably be moved to SearchByIngredients component
 
     return (
         <div className="flex flex-col items-center justify-center">
             <Routes>
-                <Route path="profile" element={!props.loggedIn ? <Navigate replace to="/" /> : <Profile username={username} setUsername={setUsername} token={token} setToken={setToken} setLoggedIn={props.setLoggedIn} savedCocktails={savedCocktails} />}></Route>
+                <Route path="profile" element={!props.loggedIn ? <Navigate replace to="/" /> : <Profile username={username} setUsername={setUsername} token={token} setToken={setToken} setLoggedIn={props.setLoggedIn} savedCocktails={savedCocktails} myBar={myBar} setMyBar={setMyBar} />}></Route>
                 <Route path="login" element={props.loggedIn ? <Navigate replace to="/" /> : <Login setToken={setToken} setLoggedIn={props.setLoggedIn} setUsername={setUsername} />}></Route>
                 <Route path=":cocktailId" element={<CocktailDetail loggedIn={props.loggedIn} username={username} token={token} savedCocktails={savedCocktails} setSavedCocktails={setSavedCocktails} />}></Route>
                 <Route path="/" element={<Search username={username} token={token} loggedIn={props.loggedIn} myBar={myBar} searchByName={searchByName} setMyBar={setMyBar} setSearchByName={setSearchByName} setSavedCocktails={setSavedCocktails} />}></Route>
